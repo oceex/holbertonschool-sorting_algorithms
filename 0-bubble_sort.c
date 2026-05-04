d95cca8134189a8178ae7265dc3614c62c1e7046
d404cjebq <imdhaydhw@gmail.com>
2026-05-04 16:24:40 +0300

    add the  big O and bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * bubble_sort - Sorts an array of integers in ascending order
  *               using the Bubble sort algorithm
@@ -8,23 +7,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
-
-    if (!array || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+size_t i, j;
+int tmp;
+if (!array || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+}
+}
+}
 }
