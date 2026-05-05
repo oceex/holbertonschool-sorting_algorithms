9c3eb16bda3b1e87426561570ea9fe5cdeb8d2b3
leen <leenalgraawi@gmail.com>
2026-05-05 17:26:24 +0300

    partitions

---
+++ 3-quick_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+void quick_sort(int *array, size_t size)
+{
+int i = -1, j = 0;
+
+while (i < j)
+quicky(array, i, j, j - i + 1);
+
+}
+void quicky(int *array, int i, int j, int size)
+{
+int prv = size - 1, tmp = 0;
+while (j < size - 1)
+{
+if (array[j] < array[prv])
+{
+i++;
+tmp = array[j];
+array[j] = array[i];
+array[i] = tmp;
+}
+j++;
+}
+quicky(array, i - 1, j, j - i + 1);
+}
