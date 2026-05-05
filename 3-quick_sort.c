f523f992158ffad84039988b8ff32297ae8fc321
leen <leenalgraawi@gmail.com>
2026-05-05 18:11:20 +0300

    partitions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,25 +2,35 @@
 
 void quick_sort(int *array, size_t size)
 {
-int i = -1, j = 0;
+quicky(array, 0, size - 1);
+}
+void quicky(int *array, size_t lf, size_t ri)
+{
+int piov = array[ri], tmp;
+size_t left = lf, right = ri;
 
-while (i < j)
-quicky(array, i, j, size);
+if (lf >= ri)
+return;
+
+while (lt < ri)
+{
+while (array[left] <= piov && lt < ri)
+left++;
+
+while (array[right] >= piov && lt < ri)
+right--;
+
+tmp = array[left];
+array[left] = array[right];
+array[right] = tmp;
+}
+
+tmp = array[left];
+array[left] = array[right];
+array[right] = tmp;
+
+quicky(array, lf, right - 1);
+quicky(array, left + 1, ri);
 
 }
-void quicky(int *array, int i, int j, int size)
-{
-int prv = size - 1, tmp = 0;
-while (j < size - 1)
-{
-if (array[j] < array[prv])
-{
-i++;
-tmp = array[j];
-array[j] = array[i];
-array[i] = tmp;
-}
-j++;
-}
-quicky(array, i - 1, j, j - i + 1);
-}
+
